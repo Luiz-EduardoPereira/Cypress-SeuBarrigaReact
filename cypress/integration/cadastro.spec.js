a91ecb0107a59998425f969740e4b13fff2ab10d
Luiz Eduardo <luiz.eduardop@outlook.com>
2022-07-24 23:13:06 -0300

    Finalizado o cenário de cadastro de usuário válido

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -12,7 +12,7 @@ describe('Tela de Cadastro', () => {
 
     it('Cadastrando um usuário válido', () => {
         cy.visit('/')
-        cy.get("#root >div #navbarSupportedContent > ul > li > a:contains('Registrar')").click()
+        cy.get("#root > div #navbarSupportedContent > ul > li > a:contains('Registrar')").click()
         cy.url().should('include', '/registro')
         cy.get(".jumbotron .form-group > .form-control:input[placeholder='Nome']").type(usuario.nome)
         cy.get(".jumbotron .form-group .input-group > input").type(usuario.email)
@@ -20,8 +20,6 @@ describe('Tela de Cadastro', () => {
         cy.get(".jumbotron .btn").click()
         cy.get('.toast-success').should('be.visible')
         cy.get('.toast .toast-message').should('have.text','Usuário adicionado com sucesso')
-        /*usuario = lepteste
-        email = lepteste
-        senha = lepteste*/
+        cy.url().should('include', '/login')
     })
 })
