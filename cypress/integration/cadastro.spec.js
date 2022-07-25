b7971387aa02b0eb541df8a09ecd39c2c634de3a
Luiz Eduardo <luiz.eduardop@outlook.com>
2022-07-24 21:18:36 -0300

    Refatorando algumas buscas.

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -6,11 +6,12 @@ describe('Tela de Cadastro', () => {
         cy.visit('/')
         cy.get("#root >div #navbarSupportedContent > ul > li > a:contains('Registrar')").click()
         cy.url().should('include', '/registro')
-        cy.get(".jumbotron .form-group > .form-control:input[placeholder='Nome']").type('lep4teste')
-        cy.get(".jumbotron .form-group .input-group > input").type('teste@gmail.com').type('lep4teste@gmail.com')
-        cy.get(".jumbotron .form-group > .form-control:input[placeholder='Senha']").type('lep4teste')
+        cy.get(".jumbotron .form-group > .form-control:input[placeholder='Nome']").type('lep14teste')
+        cy.get(".jumbotron .form-group .input-group > input").type('lep14teste@gmail.com')
+        cy.get(".jumbotron .form-group > .form-control:input[placeholder='Senha']").type('lep14teste')
         cy.get(".jumbotron .btn").click()
-        cy.get('.toast').should('be.visible').and('have.text', 'Usuário adicionado com sucesso')
+        cy.get('.toast-success').should('be.visible')
+        cy.get('.toast .toast-message').should('have.text','Usuário adicionado com sucesso')
         /*usuario = lepteste
         email = lepteste
         senha = lepteste*/
