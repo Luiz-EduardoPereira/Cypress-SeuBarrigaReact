aff9bdc1fd2a54b93044597a879977cda0a0c317
Luiz Eduardo <luiz.eduardop@outlook.com>
2022-07-24 22:45:28 -0300

    Criando cadastro com dados aleatórios utilizando a biblioteca faker-js.

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -1,14 +1,22 @@
 /// <reference types = "cypress" />
 
+const faker = require("faker-br")
+
+let usuario = {
+    nome: faker.name.firstName()+" "+faker.name.lastName(),
+    email: faker.internet.email(),
+    senha: faker.internet.password()
+}
+
 describe('Tela de Cadastro', () => {
 
     it('Cadastrando um usuário válido', () => {
         cy.visit('/')
         cy.get("#root >div #navbarSupportedContent > ul > li > a:contains('Registrar')").click()
         cy.url().should('include', '/registro')
-        cy.get(".jumbotron .form-group > .form-control:input[placeholder='Nome']").type('lep14teste')
-        cy.get(".jumbotron .form-group .input-group > input").type('lep14teste@gmail.com')
-        cy.get(".jumbotron .form-group > .form-control:input[placeholder='Senha']").type('lep14teste')
+        cy.get(".jumbotron .form-group > .form-control:input[placeholder='Nome']").type(usuario.nome)
+        cy.get(".jumbotron .form-group .input-group > input").type(usuario.email)
+        cy.get(".jumbotron .form-group > .form-control:input[placeholder='Senha']").type(usuario.senha)
         cy.get(".jumbotron .btn").click()
         cy.get('.toast-success').should('be.visible')
         cy.get('.toast .toast-message').should('have.text','Usuário adicionado com sucesso')
