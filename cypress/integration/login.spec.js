07a6c181437eeb18f90a6d35c462c50302f78b51
Luiz Eduardo <luiz.eduardop@outlook.com>
2022-08-01 21:35:53 -0300

    Implementado regra para deixar o log desabilitado para não mostrar a senha durante a execução

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -6,7 +6,7 @@ describe('Tela de Login', () => {
         cy.visit('/')
         cy.url().should('include','/login')
         cy.get('.jumbotron .form-group .input-group > .form-control').type(Cypress.env('login').email)
-        cy.get(".jumbotron .form-group > .form-control:input[placeholder='Senha']").type(Cypress.env('login').senha)
+        cy.get(".jumbotron .form-group > .form-control:input[placeholder='Senha']").type(Cypress.env('login').senha, {log: false})
         cy.get('.btn').click()
         cy.get('.container .toast-top-right').should('be.visible')
         cy.get('.container .toast .toast-message').should("have.text", "Bem vindo, "+Cypress.env('login').nome+"!")
