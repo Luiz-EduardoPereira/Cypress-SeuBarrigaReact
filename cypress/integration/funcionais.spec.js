6d98706e6822299c0a2d083ff7dc238fd47b3113
Luiz Eduardo <luiz.eduardop@outlook.com>
2022-08-10 23:59:07 -0300

    Criado o teste que valida a restrição de criação de conta já existente de forma bem simples e com dados fixos.

--- cypress/integration/funcionais.spec.js
+++ cypress/integration/funcionais.spec.js
@@ -4,7 +4,7 @@ import { usuario, cadastro }  from "../../cypress.env"
 
 describe('Realizando Testes Funcionais', () => {
 
-    it('Cadastrando um usuário válido', () => {
+    it('Deve realizar o cadastro um usuário válido', () => {
         cy.visit('/')
         cy.get("#root > div #navbarSupportedContent > ul > li > a:contains('Registrar')").click()
         cy.url().should('include', '/registro')
@@ -17,7 +17,7 @@ describe('Realizando Testes Funcionais', () => {
         cy.url().should('include', '/login')
     })
 
-    it('Realizando login com sucesso', () => {
+    it('Deve realizar login com sucesso', () => {
         cy.visit('/')
         cy.url().should('include','/login')
         cy.get('.jumbotron .form-group .input-group > .form-control').type(Cypress.env('login').email)
@@ -27,7 +27,7 @@ describe('Realizando Testes Funcionais', () => {
         cy.get('.container .toast .toast-message').should("have.text", "Bem vindo, "+Cypress.env('login').nome+"!")
     })
 
-    it.only('Criando uma conta', () => {
+    it('Deve criar uma conta', () => {
         cy.visit('/')
         cy.url().should('include','/login')
         cy.get('.jumbotron .form-group .input-group > .form-control').type(Cypress.env('login').email)
@@ -44,6 +44,23 @@ describe('Realizando Testes Funcionais', () => {
         cy.get('.form-group > button').click()
         cy.get('.toast .toast-message').should('have.text', 'Conta inserida com sucesso!')
         cy.get(".table > tbody > tr > td:contains('teste')").invoke('text').should('eq', 'teste')
+    })
 
+    it.only('Deve barrar a criação de uma conta já existente', () => {
+        cy.visit('/')
+        cy.url().should('include','/login')
+        cy.get('.jumbotron .form-group .input-group > .form-control').type(Cypress.env('login').email)
+        cy.get(".jumbotron .form-group > .form-control:input[placeholder='Senha']").type(Cypress.env('login').senha, {log: false})
+        cy.get('.btn').click()
+        cy.get('.container .toast-top-right').should('be.visible')
+        cy.get('.container .toast .toast-message').should("have.text", "Bem vindo, "+Cypress.env('login').nome+"!")
+        cy.get('.container .toast .toast-close-button').click()
+        cy.get('.container .toast .toast-close-button').should('not.exist')
+        cy.get('.navbar-dark > .dropdown-toggle').click()
+        cy.get(".navbar-dark [href='/contas']").should('be.visible').click()
+        cy.url().should('include','/contas')
+        cy.get('.form-group > input').type('teste')
+        cy.get('.form-group > button').click()
+        cy.get('.toast .toast-message').should('have.text', 'Erro: Error: Request failed with status code 400')
     })
 })
