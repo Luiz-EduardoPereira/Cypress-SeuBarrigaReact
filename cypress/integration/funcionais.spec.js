256714d5d8517aea08df41ec8059841068263991
Luiz Eduardo <luiz.eduardop@outlook.com>
2022-08-27 10:31:58 -0300

    Refatorado a spec principal e criada uma para cadastrado. Preparando a estrutura para finalizar o page object.

--- cypress/integration/funcionais.spec.js
+++ cypress/integration/funcionais.spec.js
@@ -5,22 +5,9 @@ import Movimentacao from "../support/pages/Movimentacao"
 import Dashboard from "../support/pages/Dashboard"
 
 
-
 describe('Realizando Testes Funcionais', () => {
     beforeEach(() => {
         Login.realizarLogin()
-    })
-
-    it.skip('Deve realizar o cadastro de um usuário válido', () => {
-        cy.get("#root > div #navbarSupportedContent > ul > li > a:contains('Registrar')").click()
-        cy.url().should('include', '/registro')
-        cy.get(".jumbotron .form-group > .form-control:input[placeholder='Nome']").type(variaveis.usuario.nome)
-        cy.get(".jumbotron .form-group .input-group > input").type(variaveis.usuario.email)
-        cy.get(".jumbotron .form-group > .form-control:input[placeholder='Senha']").type(variaveis.usuario.senha)
-        cy.get(".jumbotron .btn").click()
-        cy.get('.toast-success').should('be.visible')
-        cy.get('.toast .toast-message').should('have.text', variaveis.cadastro.msgUsuarioCadastradoComSucesso)
-        cy.url().should('include', '/login')
     })
 
     it('Deve criar uma conta', () => {
